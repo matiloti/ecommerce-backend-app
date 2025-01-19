83d4e1239552aeb439142b50f6b365cc94f52951
matiloti <matiaslotitoralli@gmail.com>
2025-01-19 14:02:52 +0100

    fix(entity): added NotNull annotation to required fields

--- src/main/java/com/matias/app/infrastructure/adapter/out/persistance/entity/PriceEntity.java
+++ src/main/java/com/matias/app/infrastructure/adapter/out/persistance/entity/PriceEntity.java
@@ -8,15 +8,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 /**
  * We could declare this class with references to classes like BrandEntity, ProductEntity, etc.
- * However, we only require this data from the repository. So, to avoid
- * fetching unnecessary data we only declare the required fields.
+ * However, we only require this data from the repository and creating @ManyToOne relationships
+ * would trigger Hibernate to fetch unnecessary data when accessing the brand and product IDs. 
+ * 
+ * So, to avoid fetching unnecessary data, we only declare the required fields (the IDs).
  */
 @Entity
-@Table(name = "price")
+@Table(name = "PRICES")
 @IdClass(PriceEntityPk.class)
 @Data
 public class PriceEntity {
@@ -36,6 +39,15 @@ public class PriceEntity {
 	@Id
 	private Integer priority;
 	
+	@NotNull
+	private String currency;
+
+	@NotNull
 	private Integer priceList;
+
+	@NotNull
 	private Double price;
+	
+	private LocalDateTime lastUpdate;
+	private String lastUpdateBy;
 }
