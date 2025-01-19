5f2ea36ab7974ffbf4de29176a9a0fc7a7d7e2f2
matiloti <matiaslotitoralli@gmail.com>
2025-01-19 21:05:18 +0100

    feat(test): changed tests to use MockMvc
    - Changed: test now uses MockMvc instead of a Java call to the
      controller
    - Changed: test arguments provider variable TEST_LIST simplified construction

--- src/test/java/com/matias/app/test/infrastructure/in/rest/provider/ProductPriceByDateArgumentsProvider.java
+++ src/test/java/com/matias/app/test/infrastructure/in/rest/provider/ProductPriceByDateArgumentsProvider.java
@@ -3,8 +3,6 @@ package com.matias.app.test.infrastructure.in.rest.provider;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -20,27 +18,28 @@ public class ProductPriceByDateArgumentsProvider implements ArgumentsProvider {
 	final static Long PRODUCT_TEST_ID = Long.valueOf(35455);
 	
 	// This list contains all requests that are going to be tested
-	final static List<ProductPriceByDateRequest> REQUEST_TEST_LIST = Stream.of(
-			ProductPriceByDateRequest.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).date(shortParse("2020-06-14T10:00:00")).build(),
-			ProductPriceByDateRequest.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).date(shortParse("2020-06-14T16:00:00")).build(),
-			ProductPriceByDateRequest.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).date(shortParse("2020-06-14T21:00:00")).build(),
-			ProductPriceByDateRequest.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).date(shortParse("2020-06-15T10:00:00")).build(),
-			ProductPriceByDateRequest.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).date(shortParse("2020-06-16T21:00:00")).build()
+	final static List<Arguments> TEST_LIST = Stream.of(
+			Arguments.of(
+					ProductPriceByDateRequest.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).date(shortParse("2020-06-14T10:00:00")).build(),
+					ProductPriceByDateResponse.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).startDate(shortParse("2020-06-14T00:00:00")).endDate(shortParse("2020-12-31T23:59:59")).priceList(Long.valueOf(1)).price(Double.valueOf(35.50)).build()
+			),
+			Arguments.of(
+					ProductPriceByDateRequest.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).date(shortParse("2020-06-14T16:00:00")).build(),
+					ProductPriceByDateResponse.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).startDate(shortParse("2020-06-14T15:00:00")).endDate(shortParse("2020-06-14T18:30:00")).priceList(Long.valueOf(2)).price(Double.valueOf(25.45)).build()
+			),
+			Arguments.of(
+					ProductPriceByDateRequest.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).date(shortParse("2020-06-14T21:00:00")).build(),
+					ProductPriceByDateResponse.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).startDate(shortParse("2020-06-14T00:00:00")).endDate(shortParse("2020-12-31T23:59:59")).priceList(Long.valueOf(1)).price(Double.valueOf(35.50)).build()
+			),
+			Arguments.of(
+					ProductPriceByDateRequest.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).date(shortParse("2020-06-15T10:00:00")).build(),
+					ProductPriceByDateResponse.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).startDate(shortParse("2020-06-15T00:00:00")).endDate(shortParse("2020-06-15T11:00:00")).priceList(Long.valueOf(3)).price(Double.valueOf(30.50)).build()
+			),
+			Arguments.of(
+					ProductPriceByDateRequest.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).date(shortParse("2020-06-16T21:00:00")).build(),
+					ProductPriceByDateResponse.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).startDate(shortParse("2020-06-15T16:00:00")).endDate(shortParse("2020-12-31T23:59:59")).priceList(Long.valueOf(4)).price(Double.valueOf(38.95)).build()
+			)
 		).toList();
-	
-	// This list contains the response for each request in the list above (in order)
-	final static List<ProductPriceByDateResponse> RESPONSE_TEST_LIST = Stream.of(
-			ProductPriceByDateResponse.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).startDate(shortParse("2020-06-14T00:00:00")).endDate(shortParse("2020-12-31T23:59:59")).priceList(Long.valueOf(1)).price(Double.valueOf(35.50)).build(),
-			ProductPriceByDateResponse.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).startDate(shortParse("2020-06-14T15:00:00")).endDate(shortParse("2020-06-14T18:30:00")).priceList(Long.valueOf(2)).price(Double.valueOf(25.45)).build(),
-			ProductPriceByDateResponse.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).startDate(shortParse("2020-06-14T00:00:00")).endDate(shortParse("2020-12-31T23:59:59")).priceList(Long.valueOf(1)).price(Double.valueOf(35.50)).build(),
-			ProductPriceByDateResponse.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).startDate(shortParse("2020-06-15T00:00:00")).endDate(shortParse("2020-06-15T11:00:00")).priceList(Long.valueOf(3)).price(Double.valueOf(30.50)).build(),
-			ProductPriceByDateResponse.builder().brandId(BRAND_TEST_ID).productId(PRODUCT_TEST_ID).startDate(shortParse("2020-06-15T16:00:00")).endDate(shortParse("2020-12-31T23:59:59")).priceList(Long.valueOf(4)).price(Double.valueOf(38.95)).build())
-		.toList();
-	
-	// This list merges both (each item contains the request and response to that request)
-	final static List<Arguments> TEST_LIST = IntStream.range(0, RESPONSE_TEST_LIST.size())
-			.mapToObj(i -> Arguments.of(REQUEST_TEST_LIST.get(i), RESPONSE_TEST_LIST.get(i)))
-			.collect(Collectors.toList());
 
 	/**
 	 * Shortcut function to parse a LocalDateTime to a DateTimeFormatter.ISO_LOCAL_DATE_TIME format
@@ -53,12 +52,6 @@ public class ProductPriceByDateArgumentsProvider implements ArgumentsProvider {
 	
 	@Override
 	public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
-		/* 
-		 * TEST_LIST is the result from merging the request and 
-		 * response lists to a single one (in order) where each 
-		 * item is an Arguments object containing the request 
-		 * and response
-		 * */
 		return TEST_LIST.stream();
 	}
 
